030a3912543a484cd1092075f5dadee9752bc25a
satish-313 <pradhansatish53@gmail.com>
2020-07-13 18:27:22 +0530

    variable

---
+++ variable.py
@@ -0,0 +1,11 @@
+# variable concept  and type 
+integer = int(input("Enter an integer : "))
+float_number = float(input("Enter a float number : "))
+string = input("Enter a string : ")
+t = True
+
+print (f"the type of integer is {type(integer)}")
+print (f"the type of float is {type(float_number)}")
+print (f"the type of string is {type(string)}")
+print (f"the boolean is {type(t)}")
+
