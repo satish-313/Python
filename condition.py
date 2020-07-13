137ec7340dbf3c0bf027b6c4502e14d4e85fadcf
satish-313 <pradhansatish53@gmail.com>
2020-07-13 18:33:49 +0530

    condition

---
+++ condition.py
@@ -0,0 +1,11 @@
+# condition 
+integer = int(input("enter an integer : "))
+
+if integer < 0 :
+    print (f"The {integer} is negative number ")
+
+elif integer > 0:
+    print(f"The {integer} is positive number ")
+
+else:
+    print(f"The {integer} is zero")
